00c02f1ae36255b3a837f72abd9dcc132b7864b5
Oluchi Nwenyi <dev@lulunwenyi.com>
2021-11-01 13:39:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def create_tweet():
 
 def tweet_message():
 
-    interval = 60 * 10
+    interval = 60 * 30
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
